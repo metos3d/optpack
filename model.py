e8f3b8e74747b17ccf3a2ac9abf9b9bb33524dbe
jpicau <jpi@informatik.uni-kiel.de>
2018-07-02 11:25:47 +0200

    added model script

---
+++ model.py
@@ -0,0 +1,73 @@
+import sys
+import os
+
+# main
+if __name__ == "__main__":
+    if len(sys.argv[:]) < 2:
+        print('''usage: python {0} [model-name]
+example:
+$> python {0} NPZ-DOP'''.format(sys.argv[0]))
+        sys.exit(0)
+
+    model_name = sys.argv[1]
+    model_list = ["N", "N-DOP", "NP-DOP", "NPZ-DOP", "NPZD-DOP"]
+    if not model_name in model_list:
+        print("Model not known ... " + model_name)
+        print("Choose from ... ", model_list)
+        print("Exiting ...")
+        sys.exit(1)
+
+    print("Preparing model suite ...            " + model_name)
+
+    if os.path.exists(model_name):
+        print("Directory already exists ...")
+        print("Exiting ...")
+        sys.exit(1)
+
+    print("Creating directory ...               {0}/".format(model_name))
+    os.system("mkdir {0}".format(model_name))
+
+    print("Creating directory ...               {0}/model/".format(model_name))
+    os.system("mkdir {0}/model/".format(model_name))
+
+    print("Copying environment file ...         {0}/model/de.uni-kiel.rz.nesh-fe.petsc-3.3-p7.opt.sh".format(model_name))
+    os.system("cd {0}/model/; cp ../../../../../../petsc/de.uni-kiel.rz.nesh-fe.petsc-3.3-p7.opt.sh .".format(model_name))
+
+    print("Compiling executable ...             {0}/model/metos3d-simpack-{0}.exe".format(model_name))
+    os.system('''
+cd {0}/model/;
+source ./de.uni-kiel.rz.nesh-fe.petsc-3.3-p7.opt.sh
+
+# links
+ln -s ../../../../../../metos3d/data/data/
+ln -s ../../../../../../metos3d/model/model/
+ln -s ../../../../../../metos3d/simpack/
+ln -s ../../../../../../metos3d/metos3d/Makefile
+
+# compile
+make BGC=model/{0} clean &> /dev/null
+make BGC=model/{0} &> /dev/null
+'''.format(model_name))
+
+    print("Copying job template ...             {0}/template.job.sh".format(model_name))
+    os.system("cp optpack/template/template.job.sh {0}/.".format(model_name))
+
+    print("Copying start template ...           {0}/template.start.py".format(model_name))
+    os.system("cp optpack/template/template.start.py {0}/.".format(model_name))
+
+    print("Copying model configuration ...      {0}/conf.yaml".format(model_name))
+    os.system("cp optpack/conf/{0}.conf.yaml {0}/conf.yaml".format(model_name))
+
+    print("Copying experiment script ...        {0}/experiment.py".format(model_name))
+    os.system("cp optpack/experiment.py {0}/.".format(model_name))
+
+    print("Preparing python scripts ...")
+    print("Creating directory ...               {0}/python/".format(model_name))
+    os.system("mkdir {0}/python/".format(model_name))
+
+    print("Copying python scripts ...")
+    os.system("cp optpack/python/*.py {0}/python/.".format(model_name))
+
+
+
+
