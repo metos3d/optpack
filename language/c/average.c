9a4c596af28a5d9e890c9f3db200ec24557dccf2
jpicau <jpi@informatik.uni-kiel.de>
2018-07-05 10:19:34 +0200

    corrected average

--- language/c/average.c
+++ language/c/average.c
@@ -13,27 +13,27 @@ PetscErrorCode average(Vec *yavg, Vec *y, context *ctx) {
     // 01 - 12, monthly
     // 13 - 16, seasonal
     // annual
-    for (i=0; i<ctx->nt; i++) VecAXPY(ctx->yavg[0], 1.0/(double)ctx->nt, y[i]);
+    for (i=0; i<ctx->nt; i++) VecAXPY(yavg[0], 1.0/(double)ctx->nt, y[i]);
     
     // monthly
-    start = 0;    for (i=0; i<240; i++) VecAXPY(ctx->yavg[1],  12.0/(double)ctx->nt, y[start+i]);
-    start = 240;  for (i=0; i<240; i++) VecAXPY(ctx->yavg[2],  12.0/(double)ctx->nt, y[start+i]);
-    start = 480;  for (i=0; i<240; i++) VecAXPY(ctx->yavg[3],  12.0/(double)ctx->nt, y[start+i]);
-    start = 720;  for (i=0; i<240; i++) VecAXPY(ctx->yavg[4],  12.0/(double)ctx->nt, y[start+i]);
-    start = 960;  for (i=0; i<240; i++) VecAXPY(ctx->yavg[5],  12.0/(double)ctx->nt, y[start+i]);
-    start = 1200; for (i=0; i<240; i++) VecAXPY(ctx->yavg[6],  12.0/(double)ctx->nt, y[start+i]);
-    start = 1440; for (i=0; i<240; i++) VecAXPY(ctx->yavg[7],  12.0/(double)ctx->nt, y[start+i]);
-    start = 1680; for (i=0; i<240; i++) VecAXPY(ctx->yavg[8],  12.0/(double)ctx->nt, y[start+i]);
-    start = 1920; for (i=0; i<240; i++) VecAXPY(ctx->yavg[9],  12.0/(double)ctx->nt, y[start+i]);
-    start = 2160; for (i=0; i<240; i++) VecAXPY(ctx->yavg[10], 12.0/(double)ctx->nt, y[start+i]);
-    start = 2400; for (i=0; i<240; i++) VecAXPY(ctx->yavg[11], 12.0/(double)ctx->nt, y[start+i]);
-    start = 2640; for (i=0; i<240; i++) VecAXPY(ctx->yavg[12], 12.0/(double)ctx->nt, y[start+i]);
+    start = 0;    for (i=0; i<240; i++) VecAXPY(yavg[1],  12.0/(double)ctx->nt, y[start+i]);
+    start = 240;  for (i=0; i<240; i++) VecAXPY(yavg[2],  12.0/(double)ctx->nt, y[start+i]);
+    start = 480;  for (i=0; i<240; i++) VecAXPY(yavg[3],  12.0/(double)ctx->nt, y[start+i]);
+    start = 720;  for (i=0; i<240; i++) VecAXPY(yavg[4],  12.0/(double)ctx->nt, y[start+i]);
+    start = 960;  for (i=0; i<240; i++) VecAXPY(yavg[5],  12.0/(double)ctx->nt, y[start+i]);
+    start = 1200; for (i=0; i<240; i++) VecAXPY(yavg[6],  12.0/(double)ctx->nt, y[start+i]);
+    start = 1440; for (i=0; i<240; i++) VecAXPY(yavg[7],  12.0/(double)ctx->nt, y[start+i]);
+    start = 1680; for (i=0; i<240; i++) VecAXPY(yavg[8],  12.0/(double)ctx->nt, y[start+i]);
+    start = 1920; for (i=0; i<240; i++) VecAXPY(yavg[9],  12.0/(double)ctx->nt, y[start+i]);
+    start = 2160; for (i=0; i<240; i++) VecAXPY(yavg[10], 12.0/(double)ctx->nt, y[start+i]);
+    start = 2400; for (i=0; i<240; i++) VecAXPY(yavg[11], 12.0/(double)ctx->nt, y[start+i]);
+    start = 2640; for (i=0; i<240; i++) VecAXPY(yavg[12], 12.0/(double)ctx->nt, y[start+i]);
     
     // seasonal
-    start = 0;    for (i=0; i<720; i++) VecAXPY(ctx->yavg[13], 4.0/(double)ctx->nt, y[start+i]);
-    start = 720;  for (i=0; i<720; i++) VecAXPY(ctx->yavg[14], 4.0/(double)ctx->nt, y[start+i]);
-    start = 1440; for (i=0; i<720; i++) VecAXPY(ctx->yavg[15], 4.0/(double)ctx->nt, y[start+i]);
-    start = 2160; for (i=0; i<720; i++) VecAXPY(ctx->yavg[16], 4.0/(double)ctx->nt, y[start+i]);
+    start = 0;    for (i=0; i<720; i++) VecAXPY(yavg[13], 4.0/(double)ctx->nt, y[start+i]);
+    start = 720;  for (i=0; i<720; i++) VecAXPY(yavg[14], 4.0/(double)ctx->nt, y[start+i]);
+    start = 1440; for (i=0; i<720; i++) VecAXPY(yavg[15], 4.0/(double)ctx->nt, y[start+i]);
+    start = 2160; for (i=0; i<720; i++) VecAXPY(yavg[16], 4.0/(double)ctx->nt, y[start+i]);
 
     return(0);
 }
